5e0125ef70254144eaf04288e0c04697fdb9dedd
omarr45 <58887202+omarr45@users.noreply.github.com>
2022-09-03 19:47:38 +0200

    stylings

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
-const io = require('socket.io')(server,{cors: {origin:"*"}})
+const io = require('socket.io')(server, { cors: { origin: '*' } });
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const bodyparser = require('body-parser');
 const mongoose = require('mongoose');
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 app.use(express.json());
 app.use(cors());
 app.use(bodyparser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.urlencoded({ extended: false }));
 const expressLayouts = require('express-ejs-layouts');
 const session = require('express-session');
@@ -24,56 +24,55 @@ const PORT = process.env.PORT || 4000;
 
 app.use(cookieParser());
 app.use(
-    session({
-        secret:"secret",
-        saveUninitialized:true,
-        resave:true
-    })
-)
-
+  session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true,
+  })
+);
 
 app.use(express.static(__dirname + '/public'));
 
 const db = require('./src/mongo setup').uri;
-mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() =>  console.log("mongo db connected "))
-    .catch(err => console.log(err));
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('mongo db connected '))
+  .catch((err) => console.log(err));
 
 app.use('/', require('./routes/orderRoutes'));
 
+server.listen(PORT, console.log('Server running on port 3000'));
 
-server.listen(PORT, console.log("Server running on port 3000"));
-
-io.on('connection',(socket)=>{
-    socket.on('قسم الفرد',(data)=>{
-        socket.broadcast.emit('قسم الفرد',data);
-    })
-    socket.on('قسم الرام',(data)=>{
-        socket.broadcast.emit('قسم الرام',data);
-    })
-    socket.on('قسم طباعه',(data)=>{
-        socket.broadcast.emit('قسم طباعه',data);
-    })
-    socket.on('قسم صباغه',(data)=>{
-        socket.broadcast.emit('قسم صباغه',data);
-    })
-    socket.on('قسم كستره',(data)=>{
-        socket.broadcast.emit('قسم كستره',data);
-    })
-    socket.on('قسم بوليش كمبكتور علي المفتوح',(data)=>{
-        socket.broadcast.emit('قسم بوليش كمبكتور علي المفتوح',data);
-    })
-    socket.on('قسم بوليش كمبكتور علي القفول',(data)=>{
-        socket.broadcast.emit('قسم بوليش كمبكتور علي القفول',data);
-    })
-    socket.on('قسم تغليف',(data)=>{
-        socket.broadcast.emit('قسم تغليف',data);
-    })
-    socket.on('قسم غسيل الطباعه',(data)=>{
-        socket.broadcast.emit('قسم غسيل الطباعه',data);
-    })
-    socket.on('قسم جاهز للاستلام',(data)=>{
-        socket.broadcast.emit('قسم جاهز للاستلام',data);
-    })
-    console.log("Order dep " + socket.id);
-})
+io.on('connection', (socket) => {
+  socket.on('قسم الفرد', (data) => {
+    socket.broadcast.emit('قسم الفرد', data);
+  });
+  socket.on('قسم الرام', (data) => {
+    socket.broadcast.emit('قسم الرام', data);
+  });
+  socket.on('قسم طباعه', (data) => {
+    socket.broadcast.emit('قسم طباعه', data);
+  });
+  socket.on('قسم صباغه', (data) => {
+    socket.broadcast.emit('قسم صباغه', data);
+  });
+  socket.on('قسم كستره', (data) => {
+    socket.broadcast.emit('قسم كستره', data);
+  });
+  socket.on('قسم بوليش كمبكتور علي المفتوح', (data) => {
+    socket.broadcast.emit('قسم بوليش كمبكتور علي المفتوح', data);
+  });
+  socket.on('قسم بوليش كمبكتور علي القفول', (data) => {
+    socket.broadcast.emit('قسم بوليش كمبكتور علي القفول', data);
+  });
+  socket.on('قسم تغليف', (data) => {
+    socket.broadcast.emit('قسم تغليف', data);
+  });
+  socket.on('قسم غسيل الطباعه', (data) => {
+    socket.broadcast.emit('قسم غسيل الطباعه', data);
+  });
+  socket.on('قسم جاهز للاستلام', (data) => {
+    socket.broadcast.emit('قسم جاهز للاستلام', data);
+  });
+  console.log('Order dep ' + socket.id);
+});
--- public/js/order.js
+++ public/js/order.js
@@ -9,61 +9,53 @@ const Dep2TypeInput = document.querySelector('.Dep2Type-input');
 const errorBox = document.querySelector('.error');
 const successBox = document.querySelector('.success');
 
-if(successBox.innerText.length){
-   successBox.classList.add('success');
-}
-else{
-   successBox.classList.remove('success');
-}
-
-if(errorBox.innerText.length){
-   errorBox.classList.add('error');
-}
-else{
-   errorBox.classList.remove('error');
+if (successBox.innerText.length) {
+  successBox.classList.add('success');
+} else {
+  successBox.classList.remove('success');
 }
 
-const sel = () =>{
-   if(Dep2TypeInput.value === 'قسم-الرام-تثبيت'){
-      Dep2Type.value = 1;
-   }
-   else
-      Dep2Type.value = 2;
+if (errorBox.innerText.length) {
+  errorBox.classList.add('error');
+} else {
+  errorBox.classList.remove('error');
 }
 
-Seba8a[0].style.display="none";
-Seba8a[1].style.display="none";
-Teba3a[0].style.display="none";
-Teba3a[1].style.display="none";
+const sel = () => {
+  if (Dep2TypeInput.value === 'قسم-الرام-تثبيت') {
+    Dep2Type.value = 1;
+  } else Dep2Type.value = 2;
+};
 
-radioTeba3a.addEventListener('click',()=>{
+Seba8a[0].style.display = 'none';
+Seba8a[1].style.display = 'none';
+Teba3a[0].style.display = 'none';
+Teba3a[1].style.display = 'none';
 
-   Seba8a[0].style.display="none";
-   Seba8a[1].style.display="none";
-   Teba3a[0].style.display="block";
-   Teba3a[1].style.display="block";
-   Teba3a[1].required = true;
-   Seba8a[1].required=false;
+radioTeba3a.addEventListener('click', () => {
+  Seba8a[0].style.display = 'none';
+  Seba8a[1].style.display = 'none';
+  Teba3a[0].style.display = 'block';
+  Teba3a[1].style.display = 'block';
+  Teba3a[1].required = true;
+  Seba8a[1].required = false;
+});
+radioSeba8a.addEventListener('click', () => {
+  Seba8a[0].style.display = 'block';
+  Seba8a[1].style.display = 'block';
+  Teba3a[0].style.display = 'none';
+  Teba3a[1].style.display = 'none';
+  Teba3a[1].required = false;
+  Seba8a[1].required = true;
+});
 
-})
-radioSeba8a.addEventListener('click',()=>{
-
-   Seba8a[0].style.display="block";
-   Seba8a[1].style.display="block";
-   Teba3a[0].style.display="none";
-   Teba3a[1].style.display="none";
-   Teba3a[1].required = false;
-   Seba8a[1].required=true;
-})
-
-const addOrder = () =>{
-   const orderNumber = orderNo.value;
-   const sel = document.querySelector('.list');
-   const type = document.querySelector('.list2');
-   let arr = [orderNumber,type.value,new Date().toLocaleString(),type.value];
-   socket.emit(sel,arr);
-}
-
+const addOrder = () => {
+  const orderNumber = orderNo.value;
+  const sel = document.querySelector('.list');
+  const type = document.querySelector('.list2');
+  let arr = [orderNumber, type.value, new Date().toLocaleString(), type.value];
+  socket.emit(sel, arr);
+};
 
 const socket = io('http://localhost:4000');
 socket.on('connection');
